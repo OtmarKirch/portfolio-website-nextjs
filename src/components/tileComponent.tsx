63fb2bdea2b6ed0a8794f0c70360aa1802723156
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2024-09-09 15:59:28 +0200

    Adjust tile component

--- src/components/tileComponent.tsx
+++ src/components/tileComponent.tsx
@@ -1,13 +1,15 @@
-import clsx from 'clsx';
+import Link from "next/link";
+
 
 interface TileProps {
     id: number;
     title: string;
     description: string;
     bgImageUrl: string;
+    link: string;
 }
 
-export default function TileComponent({ id, title, description, bgImageUrl }: TileProps) {
+export default function TileComponent({ id, title, description, bgImageUrl, link }: TileProps) {
     const tileStyle = {
         backgroundImage: `url(${bgImageUrl})`,
     };
@@ -15,55 +17,61 @@ export default function TileComponent({ id, title, description, bgImageUrl }: Ti
     switch (id%4) {
         case 1:
             return (
-                <div
+                <Link
+                    href={link}
                     style={tileStyle}
-                    className="h-96 bg-contain bg-center bg-no-repeat col-span-2"
+                    className="h-96 bg-contain bg-center bg-no-repeat col-span-3 md:col-span-2 hover:scale-105 transition duration-500 ease-in-out"
                 >
-                    <div className="bg-red-800/80 hover:bg-red-800/50 size-full">
-                        <h2>{title}</h2>
-                    <p>{description}</p>
+                    <div className="[&:not(:hover)]:bg-red-800/80 hover:bg-gradient-to-br from-red-800/80 size-full">
+                    <div className="">
+                        <h2 className="text-white text-xl p-2">{title}</h2>
+                        <p className="text-white p-2">{description}</p>
+                        </div>
                     </div>
                     
-                </div>
+                </Link>
             );
         case 2:
             return (
-                <div
+                <Link
+                    href={link}
                     style={tileStyle}
-                    className="h-96 bg-contain bg-center bg-no-repeat"
+                    className="h-96 bg-contain bg-center bg-no-repeat col-span-3 md:col-span-1 hover:scale-105 transition duration-500 ease-in-out"
                 >
-                    <div className="bg-blue-800/80 hover:bg-blue-800/50 size-full">
+                    <div className="[&:not(:hover)]:bg-blue-800/80 hover:bg-gradient-to-br from-blue-800/80 size-full">
                         <h2>{title}</h2>
                     <p>{description}</p>
                     </div>
                     
-                </div>
+                </Link>
             );
         case 3:
             return (
-                <div
+                <Link
+                    href={link}
                     style={tileStyle}
-                    className="h-96 bg-contain bg-center bg-no-repeat"
+                    className="h-96 bg-cover bg-center bg-no-repeat col-span-3 md:col-span-1 hover:scale-105 transition duration-500 ease-in-out"
                 >
-                    <div className="bg-green-800/80 hover:bg-green-800/50 size-full">
+                    <div className="[&:not(:hover)]:bg-green-800/80 hover:bg-gradient-to-br from-green-800/80 size-full">
                         <h2>{title}</h2>
                     <p>{description}</p>
                     </div>
                     
-                </div>
+                </Link>
             );
         case 0:
             return (
-                <div
+                <Link
+                    href={link}
                     style={tileStyle}
-                    className="h-96 bg-contain bg-center bg-no-repeat col-span-2"
+                    className="h-96 bg-cover bg-center bg-no-repeat col-span-3 md:col-span-2 hover:scale-105 transition duration-500 ease-in-out"
                 >
-                    <div className="bg-yellow-800/80 hover:bg-yellow-800/50 size-full">
+                    <div className="[&:not(:hover)]:bg-yellow-800/80 hover:bg-gradient-to-br from-yellow-800/80 size-full">
                         <h2>{title}</h2>
                     <p>{description}</p>
                     </div>
                     
-                </div>
+                </Link>
             );
     }
 

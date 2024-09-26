9e88c722398c7074ae769bc68c37a45fbe964f8b
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2024-09-26 13:23:57 +0200

    Adjust icon color

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -13,14 +13,14 @@ export default function FooterComponent() {
 
                 <div className="group relative h-full hover:scale-105 duration-1000">
                     <Link className="h-full" href="https://www.linkedin.com/in/otmar-kirchg%C3%A4%C3%9Fner-6196b18a/" target="_blank">
-                        <img className="h-full" src="img/linkedin_icon.svg" alt="" />
+                        <img className="h-full" src="img/linkedin_icon_grey.svg" alt="" />
                     </Link>
                     <div className="absolute -z-10 inset-0 opacity-0 group-hover:opacity-100 bg-gradient-to-tl from-blue-800 to-blue-500 transition-opacity duration-500 rounded-sm md:rounded-md"></div>
                 </div>
 
                 <div className="group relative h-full md:p-1 xl:p-2 hover:scale-105 duration-1000">
                     <Link className="h-full" href="https://github.com/OtmarKirch" target="_blank">
-                        <img className="h-full" src="img/github_icon_black.svg" alt="" />
+                        <img className="h-full" src="img/github_icon_grey.svg" alt="" />
                     </Link>
                     <div className="absolute -z-10 inset-0 opacity-0 group-hover:opacity-100 bg-gradient-to-tl from-blue-800 to-blue-500 transition-opacity duration-500 rounded-full"></div>
                 </div>
@@ -29,7 +29,7 @@ export default function FooterComponent() {
             <div className="h-6 md:h-12 xl:h-24 flex flex-row justify-end items-center gap-4 md:gap-6 xl:gap-8">
                 <div className="group relative h-full md: p-1 xl:p-2 hover:scale-105 duration-1000">
                     <Link className="h-full" href="mailto:otmar.kirchgaessner@gmail.com">
-                        <img src="img/email_icon.svg" alt="" className="h-full" />
+                        <img src="img/email_icon_grey.svg" alt="" className="h-full" />
                     </Link>
                     <div className="absolute -z-10 inset-0 opacity-0 group-hover:opacity-100 bg-gradient-to-tl from-blue-800 to-blue-500 transition-opacity duration-500"></div>
                 </div>

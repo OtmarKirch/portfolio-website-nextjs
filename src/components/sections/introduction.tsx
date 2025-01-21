4fb5d5d15c2a1c0f4b2c1e59984350ae4e7d9a6f
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2025-01-21 15:33:49 +0100

    Add resume

--- src/components/sections/introduction.tsx
+++ src/components/sections/introduction.tsx
@@ -8,7 +8,7 @@ export default function Introduction() {
                 <h1 className="sm:hidden text-center text-white">About Me</h1>
                 <div className="flex justify-center items-center">
                     <Image width={1024} height={1024} src={"/img/Otmar_rectangle_small.jpg"} alt="Image Otmar Kirchgäßner"
-                    className="w-24 md:w-44 xl:w-72 h-24 md:h-44 xl:h-72 m-4 md:m-12 xl:m-24 rounded-full shadow-lg shadow-white"
+                        className="w-24 md:w-44 xl:w-72 h-24 md:h-44 xl:h-72 m-4 md:m-12 xl:m-24 rounded-full shadow-lg shadow-white"
                     />
                     <div className="mr-4 md:mr-12 xl:mr-24">
                         <h1 className="font-mulish hidden sm:block md:mb-4">About Me</h1>
@@ -16,10 +16,20 @@ export default function Introduction() {
                             <p className="custom-text">
                                 I am a passionate full-stack <span>&lt; web &gt;</span> developer from Germany with comprehensive expertise in creating and maintaining web applications, and a passion for test-driven development. Experienced in both collaborative environments and detail-oriented, independent work, consistently ensuring reliable and timely delivery.
                             </p>
+
+                            <a
+                                href="/files/Otmar_CV.pdf"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="underline mt-4 inline-block hover:text-blue-500 transition-color duration-500"
+                            >
+                                See Resume
+                            </a>
                         </p>
                     </div>
                 </div>
             </div>
+
         </div>
     )
 }

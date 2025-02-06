63a0f5c2b47b62689ca1545e4afb311418ebcdfd
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2025-02-06 15:44:53 +0100

    Add Codewars logo

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'www.codewars.com',
+                port: '',
+                pathname: '/users/OtmarKirch/badges/micro',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,4 +1,6 @@
 import Navigation from "../navigation";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Hero() {
     return (
@@ -7,13 +9,24 @@ export default function Hero() {
             <div className="absolute top-2 sm:top-4 md:top-8 xl:top-10 left-4 md:left-6 xl:left-8 custom-text">
                 <Navigation />
             </div>
-            
+
             <div className="absolute inset-y-1/2 left-4 md:left-6 xl:left-8 flex flex-col justify-center items-center">
                 <h1 className="w-24 sm:w-48 md:w-72 xl:w-96 pl-1 sm:p-2 md:p-4 xl:p-8 border-2 border-white text-lg sm:text-2xl md:text-4xl xl:text-7xl text-white font-mulish uppercase">
                     Living Web Design on the Edge
                 </h1>
             </div>
-            <p className="absolute  left-4 md:left-6 xl:left-8 bottom-2 sm:bottom-4 md:bottom-8 xl:bottom-10 text-white custom-text">Welcome to my Website</p>
+            <p className="absolute left-4 md:left-6 xl:left-8 bottom-2 sm:bottom-4 md:bottom-8 xl:bottom-10 text-white custom-text">Welcome to my Website</p>
+            <div className="w-16 md:w-20 xl:w-24 p-0.5 absolute right-4 md:right-4 xl:right-8 bottom-2 sm:bottom-4 md:bottom-8 xl:bottom-10">
+                <Link href="https://www.codewars.com/users/OtmarKirch" target="_blank">
+                    <Image
+                        width={100} // default width
+                        height={10} // default height
+                        src={"https://www.codewars.com/users/OtmarKirch/badges/micro"}
+                        alt="Codewars rank and honor points"
+                        unoptimized={true}
+                    />
+                </Link>
+            </div>
             <img src="/img/hero-image.png" alt="hero" className="w-full" />
         </div>
     )
